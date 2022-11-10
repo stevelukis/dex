12e54e0c1281866fa8b3cb647f2142ab78870de3
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-10 08:49:27 +0800

    Change to using single quote for string

--- test/Exchange.ts
+++ test/Exchange.ts
@@ -1,24 +1,24 @@
-import { ethers } from "hardhat";
-import { ContractFactory } from "ethers";
-import { Exchange } from "../typechain-types";
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { expect } from "chai";
+import { ethers } from 'hardhat';
+import { ContractFactory } from 'ethers';
+import { Exchange } from '../typechain-types';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
 
 const ethToWei = (n: number | string) => {
     return ethers.utils.parseUnits(n.toString(), 'ether');
 }
 
-describe("Exchange", function () {
+describe('Exchange', function () {
     const deployExchangeFixture = async () => {
         const [owner, feeAccount, user1] = await ethers.getSigners();
 
         const feePercent = 10;
 
-        const Exchange: ContractFactory = await ethers.getContractFactory("Exchange");
+        const Exchange: ContractFactory = await ethers.getContractFactory('Exchange');
         const exchangeContract = await Exchange.deploy(feeAccount.address, feePercent);
-        const exchange: Exchange = await ethers.getContractAt("Exchange", exchangeContract.address);
+        const exchange: Exchange = await ethers.getContractAt('Exchange', exchangeContract.address);
 
-        const Token = await ethers.getContractFactory("Token");
+        const Token = await ethers.getContractFactory('Token');
         const token1Contract = await Token.deploy('Dapp University', 'DAPP', ethToWei('1000000'));
         const token1 = await ethers.getContractAt('Token', token1Contract.address);
 
@@ -28,12 +28,12 @@ describe("Exchange", function () {
     };
 
     describe('Deployment', function () {
-        it("Should return the correct fee account", async function () {
+        it('Should return the correct fee account', async function () {
             const { exchange, feeAccount } = await loadFixture(deployExchangeFixture);
             expect(await exchange.feeAccount()).to.be.equal(feeAccount.address);
         });
 
-        it("Should return the correct fee percentage", async function () {
+        it('Should return the correct fee percentage', async function () {
             const { exchange, feePercent } = await loadFixture(deployExchangeFixture);
             expect(await exchange.feePercent()).to.be.equal(feePercent);
         });
