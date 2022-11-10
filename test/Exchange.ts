439d1190e0243068a58bb355702fdac15989b9dd
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-10 09:25:20 +0800

    Add deposit function

--- test/Exchange.ts
+++ test/Exchange.ts
@@ -24,7 +24,7 @@ describe('Exchange', function () {
 
         await token1.transfer(user1.address, ethToWei(100))
 
-        return { owner, feeAccount, feePercent, exchange, token1 };
+        return { owner, feeAccount, user1, feePercent, exchange, token1 };
     };
 
     describe('Deployment', function () {
@@ -39,4 +39,33 @@ describe('Exchange', function () {
         });
     });
 
+    describe('Depositing Tokens', function () {
+        describe('Success', function () {
+            async function depositTokenFixture() {
+                const deployStuffs = await loadFixture(deployExchangeFixture);
+                const { exchange, token1, user1 } = deployStuffs;
+
+                const amount = ethToWei(10);
+
+                await token1.connect(user1).approve(exchange.address, amount);
+                await exchange.connect(user1).depositToken(token1.address, amount);
+                return { ...deployStuffs, amount }
+            }
+
+            it('Should transfer the token to the exchange and save the balance', async function () {
+                const { exchange, token1, amount, user1 } = await loadFixture(depositTokenFixture);
+
+                expect(await token1.balanceOf(exchange.address)).to.equal(amount);
+                expect(await exchange.tokens(token1.address, user1.address)).to.equal(amount);
+                expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount);
+            });
+        });
+        
+        describe('Failure', function () {
+            it("Should be reverted when no tokens are approved", async function () {
+                const {exchange, user1, token1} = await loadFixture(deployExchangeFixture);
+                await expect(exchange.connect(user1).depositToken(token1.address, 10)).to.be.reverted;
+            })
+        });
+    });
 });
